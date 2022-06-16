063af817fdc873fa6a7263859f579c97cabf22b4
Inbal Prital <inbalprital@gmail.com>
2022-06-16 18:35:12 +0300

    my six commit

--- js/galleryController.js
+++ js/galleryController.js
@@ -1,23 +1,30 @@
 function showGallery() {
   var elGallery = document.querySelector('.gallery')
-  elGallery.style.display = 'flex'
+  elGallery.style.display = 'grid'
   var elEditor = document.querySelector('.editor')
   elEditor.style.display = 'none'
 }
 
-function renderGallery() {
-  // var images = getImages()
-  var strHtml = `<img src="images/1.jpg" id="1" class="images img-1" onclick="onImgSelect(this)" />
- <img src="images/2.jpg" id="2" class="images img-2" onclick="onImgSelect(this)" />`
-  document.querySelector('.gallery').innerHTML += strHtml
-}
-
-// var gImgs = [
-//   { id: 1, url: 'imges/1.jpg', keywords: ['funny', 'cat'] },
-//   { id: 2, url: 'imges/2.jpg', keywords: ['funny', 'cat'] },
+// function renderGallery() {
+//   // var images = getImages()
+//   var strHtml = `<img src="images/1.jpg" id="1" class="images img-1" onclick="onImgSelect(this)" />
+//  <img src="images/2.jpg" id="2" class="images img-2" onclick="onImgSelect(this)" />`
+//   document.querySelector('.gallery').innerHTML += strHtml
+// }
 
 function onImgSelect(img) {
   setImage(img)
   renderMeme()
   showEditor()
+}
+
+function renderGallery() {
+  var images = getImages()
+  console.log('images', images)
+
+  var strHtml = images.map(
+    (image) =>
+      `<img src="images/${image.id}.jpg" id="${image.id}" class="images img-${image.id}" onclick="onImgSelect(this)" />`
+  )
+  document.querySelector('.gallery').innerHTML += strHtml.join('')
 }
